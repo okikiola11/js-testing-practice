4c6240c23efed8d9bb536ff8b13776810c6107f4
okikiola11 <okikiola.apelehin@gmail.com>
2021-03-14 19:29:49 +0000

    Add calculator subtraction logic

--- index.js
+++ index.js
@@ -13,7 +13,30 @@ const calculator = () => {
   const add = (...arg) => {
     return arg.reduce((a, b) => a + b, 0);
   }
+
+  const subtract = (...arg) => {
+    let total = arg[0];
+    if (typeof (total) !== 'number') return false;
+    length = arg.length;
+    for (let i = 1; i < length; i++) {
+      if (typeof (arg[i]) == 'number') {
+        total -= arg[i];
+      } else {
+        return false;
+      }
+    }
+    return total;
+  }
+
+  const divide = () => {
+    
+  }
+
+  const multiply = () => {
+    
+  }
   console.log(add(1,3,4,5))
+  console.log(subtract(7,3,2,-1,10))
 }
 
 calculator();
